a80ffafb200adf02e93b2acb9af5e25a7801ed0d
Weronika <weronikalimberger@gmail.com>
2023-03-15 18:06:45 +0000

    add two more routes to the home def

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,9 @@ from flask import render_template
 from application import app
 
 
+@app.route('/')
 @app.route('/home')
+@app.route('/index')
 def home():
     title_name = "Home"
     return render_template('home.html', title=title_name)
