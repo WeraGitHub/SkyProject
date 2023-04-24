3fef602a3a4a56e657903418ad39276cccca1304
Weronika <weronikalimberger@gmail.com>
2023-04-24 08:46:27 +0100

    Create test units

---
+++ tests/test_routes.py
@@ -0,0 +1,30 @@
+from application import app
+from application.routes import get_weather
+
+client = app.test_client()
+
+
+def test_home():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Home page' in response.data
+
+
+def test_about():
+    response = client.get('/about')
+    assert response.status_code == 200
+    assert b'Person A' in response.data
+    assert b'Person B' in response.data
+    assert b'Person C' in response.data
+
+
+def test_get_weather():
+    locations = ['Three Legged Cross', 'Isleworth', 'Chilworth']
+    weather_data_list = get_weather(locations)
+    assert len(weather_data_list) == len(locations)
+
+
+def test_pipes():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Pipelines' in response.data
